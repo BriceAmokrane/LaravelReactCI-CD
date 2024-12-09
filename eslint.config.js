3053464dd107732e50626fff4b5db3903f29e819
BriceAmokrane <briceamokranedev@gmail.com>
2024-12-09 17:53:55 +0100

    packages

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import react from "eslint-plugin-react";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+    {
+        files: ["**/*.{js,jsx}"],
+        plugins: {
+            react,
+            "jsx-a11y": jsxA11y,
+            "react-hooks": reactHooks
+        },
+        extends: [react.configs.recommended],
+        env: {
+            browser: true,
+            es2021: true
+        },
+        rules: {
+            "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
+            "react/react-in-jsx-scope": "off"
+        },
+        languageOptions: {
+            sourceType: "module",
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        }
+    }
+];
